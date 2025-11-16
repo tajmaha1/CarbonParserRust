e490a25a8f2973be6c61611bdcdd92a32d05c6c3
megatr4n <d.cherniavskyi@ukma.edu.ua>
2025-11-16 04:40:45 +0200

    Delete target directory

--- carbon-parser/target/debug/build/proc-macro2-69c4c2c87bc9cd4f/build_script_build-69c4c2c87bc9cd4f.d
+++
@@ -1,5 +0,0 @@
-/Users/daniilpanasiuk/Desktop/CarbonParserRust-2/carbon-parser/target/debug/build/proc-macro2-69c4c2c87bc9cd4f/build_script_build-69c4c2c87bc9cd4f.d: /Users/daniilpanasiuk/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.103/build.rs
-
-/Users/daniilpanasiuk/Desktop/CarbonParserRust-2/carbon-parser/target/debug/build/proc-macro2-69c4c2c87bc9cd4f/build_script_build-69c4c2c87bc9cd4f: /Users/daniilpanasiuk/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.103/build.rs
-
-/Users/daniilpanasiuk/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.103/build.rs:
--- carbon-parser/target/debug/deps/colorchoice-0e6fb0fc54ae5cde.d
+++
@@ -1,5 +0,0 @@
-/Users/daniilpanasiuk/Desktop/CarbonParserRust-2/carbon-parser/target/debug/deps/colorchoice-0e6fb0fc54ae5cde.d: /Users/daniilpanasiuk/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.4/src/lib.rs
-
-/Users/daniilpanasiuk/Desktop/CarbonParserRust-2/carbon-parser/target/debug/deps/libcolorchoice-0e6fb0fc54ae5cde.rmeta: /Users/daniilpanasiuk/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.4/src/lib.rs
-
-/Users/daniilpanasiuk/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/colorchoice-1.0.4/src/lib.rs:
